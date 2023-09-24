46e23888099113eb732818dc68066d783029d0a4
Hoafn0730 <hoantran0730@gmail.com>
2023-09-24 10:51:11 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// import Header from './components/header.js';
+
+// const root = document.getElementById('root');
+// root.innerHTML = App();
+
+// function App() {
+//     return `
+
+//     `;
+// }

21f6748558599c96b98fbf0e85aa269ade9303e8
Hoafn0730 <hoantran0730@gmail.com>
2023-10-22 10:57:50 +0700

    update search home

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,23 +1,22 @@
 var app = angular.module('AppHocTap', []);
 const searchInput = document.querySelector('.header_search-input');
 app.controller('HomeCtrl', function ($scope, $http) {
-    //     $scope.khoahoc;
-    //     $scope.GetKhoaHoc = function (name) {
-    //         $http({
-    //             method: 'port',
-    //             data: { page: 1, pageSize: 10, name },
-    //             url: API + '/api/Course/search',
-    //         }).then(function (response) {
-    //             debugger;
-    //             $scope.khoahoc = response.data;
-    //         });
-    //     };
-    //     $scope.GetKhoaHoc('n');
-    // searchInput.onchange = () => {
-    //     if (searchInput.value !== '') {
-    //         console.log(searchInput.value);
-    //     }
-    // };
-    // searchInput.oninput = () => {
-    // };
+    $scope.khoahoc;
+    $scope.SeachKhoaHoc = function (name) {
+        $http({
+            method: 'POST',
+            data: { page: 1, pageSize: 10, name },
+            url: API + '/api/Course/search',
+        }).then(function (response) {
+            // debugger;
+            $scope.listItem = response.data;
+            console.log($scope.listItem.data);
+        });
+    };
+
+    searchInput.oninput = () => {
+        if (searchInput.value !== '') {
+            $scope.SeachKhoaHoc(searchInput.value);
+        }
+    };
 });
